dd1f436236eb6f1f03d01ffc0b57d4a16c4ac568
Nathan Phennel <nathanphennel@gmail.com>
2023-12-30 13:23:17 +1100

    feat: cleanup and remove lame cows

--- cli.js
+++ cli.js
@@ -10,11 +10,8 @@ const path = require("path");
 program
   .description("banter - lighthearted remarks for when you need to break up the monotony")
   .version(package.version, "-v, --version")
-  .option("-l, --list", "list all the jokes")
-  .option("-t, --tag <value>", "filter banter with tag")
-  .option("-j, --jokes", "return jokes only (WIP)")
-  .option("-q, --quotes", "return quotes only (WIP)")
-  .option("-a, --add", "interactively add a record (WIP)")
+  .option("-l, --list", "list all the banter")
+  .option("-t, --tag <value>", "filter banter using a tag")
   .on("--help", () => {
     console.log("\nExamples:");
     ["", "--tag react", "-t javascript", "--list", "-l", "-lt react"].forEach(cmd => console.log(`  $ banter ${cmd}`));
@@ -32,12 +29,28 @@ fs.readdir(route, (err, files) => {
       const cowContent = fs.readFileSync(cowPath, 'utf8');
       return cowContent.length <= 1100 && cowContent.length >= 100 &&
         // cow content doesn't contain 'you'
-        cowContent.indexOf("you") === -1;
+        cowContent.indexOf("you") === -1 &&
+        cowContent.indexOf(" ;=") === -1 &&
+        cowContent.indexOf(", .+X") === -1 &&
+        cowContent.indexOf(". .)(,") === -1 &&
+        cowContent.indexOf(":HHHHHHHHHHHHHHHHHHX,") === -1 &&
+        cowContent.indexOf("ζ, ヘ /::(___)|/:(￣￣ )") === -1 &&
+        cowContent.indexOf("X MM X") === -1 &&
+        cowContent.indexOf("=-=:=") === -1 &&
+        cowContent.indexOf(",;X@@X%/;=----=:/%X@@X/,") === -1 &&
+        cowContent.indexOf(".,-, .-,.") === -1 &&
+        cowContent.indexOf("OOOOOOOOOOOOOOOOOOOOOOOOO") === -1 &&
+        cowContent.indexOf("Skywalker") === -1 &&
+        cowContent.indexOf("Vader") === -1 &&
+        cowContent.indexOf("BECAUSE") === -1 &&
+        cowContent.indexOf(";;;ﾌ") === -1 &&
+        cowContent.indexOf("International Business Machines") === -1 &&
+        cowContent.indexOf("####$=") === -1;
     }).map(file => file.slice(0, -4));
 
     const randomCow = possibleCows[Math.floor(Math.random() * possibleCows.length)] || "stegosaurus";
     const output = program.list ? banter.list(program.tag).join("\n") : cowsay.say({
-      text: banter.random(program.tag),
+      text: "\n" + banter.random(program.tag) + "\n\n",
       W: 50,
       f: randomCow,
     });
